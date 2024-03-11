9dfa873e1decffb18c5a5e26a6bcadc0deafe3f5
Ikethepro18 <ikethepro18@gmail.com>
2024-03-11 21:27:12 +0000

    Updated fabric.mod.json and some other stuff

---
+++ src/main/java/ike/itd/IntoTheDarkClient.java
@@ -0,0 +1,10 @@
+package ike.itd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IntoTheDarkClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
