b3ef64732fae07f36dddf8157abab28b73cef152
Ikethepro18 <ikethepro18@gmail.com>
2024-03-14 23:01:11 +0000

    Tweaked some stuff, getting the project to a base ready to start level. Ideally this can be used as a fallback in case I break everything in the future but who knows how it'll be. You can consider this V0.0.1 of Into the Dark

---
+++ src/client/java/ike/itd/IntoTheDarkDataGenerator.java
@@ -0,0 +1,11 @@
+package ike.itd;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IntoTheDarkDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
